6e0571a9b34baac5be11380520eea097640c92bb
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-08-25 22:25:23 +0300

    02. usefull-things-271-typing. UI. Typed js.

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var typed = new Typed(".type", {
+        strings: [
+            "<h1>Hello for all!</h1>",
+            "<h1>I'm from Ukraine</h1>",
+            "<h1>Learn Web Developing more than 5 years</h1>",
+            "<h1>Please, subscribe and leave comment</h1>",
+        ],
+        typeSpeed: 70,
+        backDelay: 200,
+        loop: true,
+    });
+});
